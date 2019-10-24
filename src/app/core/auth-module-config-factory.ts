490060fc9e5c883cbd6c740da644a9e4f7c9fdf8
davebaol <davix7@yahoo.it>
2019-10-24 15:11:12 +0200

    Clarification on custom URL validation

--- src/app/core/auth-module-config-factory.ts
+++ src/app/core/auth-module-config-factory.ts
@@ -4,8 +4,15 @@ import { ConfigService } from './config.service';
 export function authModuleConfigFactory(configService: ConfigService): OAuthModuleConfig {
   return {
     resourceServer: {
+      // Url prefixes for which calls should be intercepted 
       allowedUrls: [`${configService.apiUrl}`],
-      sendAccessToken: true,
+
+      // If specified as a funtion of type (url: string) => boolean
+      // it will be used in place of the allowedUrls whitelist.
+      customUrlValidation: undefined,
+
+      // If true access token is attached to request headers
+      sendAccessToken: true
     }
   };
 }
